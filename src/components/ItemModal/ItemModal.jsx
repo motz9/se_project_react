6e7c18ac1085aafbf7e7df65682d93a6a7935546
Motz <mjlane88@gmail.com>
2025-05-04 14:23:39 -0500

    Update ItemModal styles

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,21 +1,22 @@
 import "./ItemModal.css";
-import closebtn from "../../assets/closebtn.svg";
+import "../ModalWithForm/ModalWithForm.css";
+import lightclosebtn from "../../assets/lightclosebtn.svg";
 
 function ItemModal({ activeModal, card, onClose }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-      <div className="modal__content modal__content_type_image">
+    <div className={`modal ${activeModal === "preview" ? "modal_opened" : ""}`}>
+      <div className="modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
           <img
             className="modal__close-button"
-            src={closebtn}
+            src={lightclosebtn}
             alt="close-button"
           />
         </button>
         <img src={card.link} alt="" className="modal__image" />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
-          <p className="modal__weather">Weather: {card.weather}</p>
+          <h2 className="modal__text-base modal__caption">{card.name}</h2>
+          <p className="modal__text-base modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
     </div>
