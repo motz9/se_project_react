c7f0f701fd2ae3a5fdc1aff68ccf4c9e9e5d12c9
Motz <mjlane88@gmail.com>
2025-05-06 23:26:09 -0500

    Update WeatherCards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,45 +1,117 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "few clouds",
+    url: new URL("../assets/day/partly_cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "shower rain",
+    url: new URL("../assets/day/rainy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "thunderstorm",
+    url: new URL("../assets/day/thunderstorm.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snowy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "overcast clouds",
+    url: new URL("../assets/day/cloudy", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "few clouds",
+    url: new URL("../assets/night/partly_cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "shower rain",
+    url: new URL("../assets/night/rainy.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "thunderstorm",
+    url: new URL("../assets/night/thunderstorm.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snowy.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "overcast clouds",
+    url: new URL("../assets/night/cloudy", import.meta.url).href,
+  },
+];
+
+export const defaultWeatherOptions = {
+  day: {
+    url: new URL("../assets/day/default.svg", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../assets/night/default.svg", import.meta.url).href,
+  },
+};
+
 export const defaultClothingItems = [
-    {
-      _id: 0,
-      name: "Cap",
-      weather: "hot",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
-    },
-    {
-      _id: 1,
-      name: "Hoodie",
-      weather: "warm",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
-    },
-    {
-      _id: 2,
-      name: "Jacket",
-      weather: "cold",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
-    },
-    {
-      _id: 3,
-      name: "Sneakers",
-      weather: "cold",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
-    },
-    {
-      _id: 4,
-      name: "T-Shirt",
-      weather: "hot",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
-    },
-    {
-      _id: 5,
-      name: "Coat",
-      weather: "cold",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
-    },
-  ];
+  {
+    _id: 0,
+    name: "Cap",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+  },
+  {
+    _id: 1,
+    name: "Hoodie",
+    weather: "warm",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+  },
+  {
+    _id: 2,
+    name: "Jacket",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+  },
+  {
+    _id: 3,
+    name: "Sneakers",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
+  },
+  {
+    _id: 4,
+    name: "T-Shirt",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+  },
+  {
+    _id: 5,
+    name: "Coat",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+  },
+];
 
-  export const coordinates = {
-    latitude: 42.893291,
-    longitude: -93.215302,
-  }
+export const coordinates = {
+  latitude: 42.893291,
+  longitude: -93.215302,
+};
 
-  export const APIkey = "3971e019797917027215a651842e14c0";
+export const APIkey = "3971e019797917027215a651842e14c0";
